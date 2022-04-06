96aae6185cc6aa2c581ed986ff87c91d199b6e12
SachinSingh <90815688+HeistHub@users.noreply.github.com>
2022-04-06 12:47:33 +0530

    Add files via upload

---
+++ sortingarray.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int selestion_sorting(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+    }
+    return arr[n];
+}
+int bubble_sorting(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr[n];
+}
+int insertion_sorting(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int current=arr[i];
+        int j=i-1;
+        while(arr[j]>current && j>=0)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    arr[n]=bubble_sorting(arr,n);
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
